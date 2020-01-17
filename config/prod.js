18afe6ffff1498d2756c6eac1917717f5fbffe9d
IvgNarinsky <33311257+IvgNarinsky@users.noreply.github.com>
2020-01-17 14:09:47 +0200

    make it better

--- config/prod.js
+++ config/prod.js
@@ -1,7 +1,6 @@
 
 module.exports={
-    // MONGO_DB:process.env.MONGO_DB ,
-    MONGO_DB:"mongodb+srv://ivgeni:burger1234@cluster0-djgnk.mongodb.net/test?retryWrites=true&w=majority",
-    SECRET:'im awasome and you know it'
+    MONGO_DB:process.env.MONGO_DB 
+    
    
 }
--- index.js
+++ index.js
@@ -3,7 +3,9 @@ const app=express()
 const bodyParser=require("body-parser")
 const mongoose=require('mongoose')
 const keys=require("./config/keys")
+const dotenv = require("dotenv");
 
+dotenv.config();
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 mongoose.Promise=global.Promise
